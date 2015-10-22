47f1e59b3d6175140fc43368a0b516a09a52e1c4
aangurets <andrei.angurets@gmail.com>
2015-10-22 12:42:56 +0300

    removed the "Code" field of the display list

--- Bookstore/app/src/main/java/com/example/bookstore/adapter/BookAdapter.java
+++ Bookstore/app/src/main/java/com/example/bookstore/adapter/BookAdapter.java
@@ -40,18 +40,15 @@ public class BookAdapter extends ArrayAdapter<Book> {
         if (mView == null) {
             mView = LayoutInflater.from(parent.getContext()).inflate(R.layout.book, parent, false);
             holder = new ViewHolder();
-            holder.mCodeTextVew = (TextView) mView.findViewById(R.id.code_field);
             holder.mAuthorTextView = (TextView) mView.findViewById(R.id.author_field);
             holder.mNameTextVew = (TextView) mView.findViewById(R.id.name_field);
             mView.setTag(holder);
         } else {
             holder = (ViewHolder) mView.getTag();
         }
-        String mCode = Constants.CODE + getItem(position).getCode();
         String mAuthor = Constants.AUTHOR + getItem(position).getAuthor();
         String mName = Constants.NAME + getItem(position).getName();
 
-        holder.mCodeTextVew.setText(mCode);
         holder.mAuthorTextView.setText(mAuthor);
         holder.mNameTextVew.setText(mName);
 
@@ -59,7 +56,6 @@ public class BookAdapter extends ArrayAdapter<Book> {
     }
 
     static class ViewHolder {
-        public TextView mCodeTextVew;
         public TextView mAuthorTextView;
         public TextView mNameTextVew;
     }
