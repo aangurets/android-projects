6c2d434f4ddb03dc727f7645b2562ff54525279e
Andrei Angurets <andrei.angurets@gmail.com>
2015-10-28 23:08:52 +0300

    model item

---
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/Item.java
@@ -0,0 +1,18 @@
+package by.aangurets.rssreader.model;
+
+import java.util.Date;
+
+/**
+ * Created by andrei.angurets on 28/10/2015.
+ */
+public class Item {
+
+    private String mTitle;
+    private String mDescription;
+    private String mLink;
+    private String mGuid;
+    private Date mPubDate;
+    private String mImageLink;
+    private String mCategory;
+
+}
