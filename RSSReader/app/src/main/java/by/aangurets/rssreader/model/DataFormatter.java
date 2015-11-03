17865a70edb0691ef81bf921e47abea1128655bc
Andrei Angurets <andrei.angurets@gmail.com>
2015-11-03 23:07:30 +0300

    DataFormatter class

---
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/DataFormatter.java
@@ -0,0 +1,11 @@
+package by.aangurets.rssreader.model;
+
+/**
+ * Created by andrei.angurets on 03/11/2015.
+ */
+public class DataFormatter {
+
+    public static String formattingDate(String date) {
+
+    }
+}
