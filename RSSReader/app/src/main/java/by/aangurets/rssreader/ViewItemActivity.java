f7c70c479d2e0152d71363bdf8f7946bdc7ef01c
Andrei Angurets <andrei.angurets@gmail.com>
2015-11-07 11:33:46 +0300

    view item activity class & xml

--- RSSReader/app/src/main/java/by/aangurets/rssreader/ViewItemActivity.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/ViewItemActivity.java
@@ -1,7 +1,17 @@
 package by.aangurets.rssreader;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by andrei.angurets on 07/11/2015.
  */
-public class ViewItemActivity {
+public class ViewItemActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_item);
+
+    }
 }
