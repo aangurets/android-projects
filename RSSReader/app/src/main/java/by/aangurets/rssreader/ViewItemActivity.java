601c96e379de2d880437fa8de61ae33190f50d95
Andrei Angurets <andrei.angurets@gmail.com>
2015-11-08 23:13:26 +0300

    view item activity

--- RSSReader/app/src/main/java/by/aangurets/rssreader/ViewItemActivity.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/ViewItemActivity.java
@@ -3,12 +3,31 @@ package by.aangurets.rssreader;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.androidquery.AQuery;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import by.aangurets.rssreader.model.Item;
+import by.aangurets.rssreader.storage.ItemsStorage;
 
 /**
  * Created by andrei.angurets on 07/11/2015.
  */
 public class ViewItemActivity extends Activity {
 
+    @Bind(R.id.titleInViewItem)
+    TextView mTitle;
+
+    @Bind(R.id.dateInViewItem)
+    TextView mDate;
+
+    @Bind(R.id.descriptionInViewItem)
+    TextView mDescription;
+
+    AQuery mAquery = new AQuery(this);
     int mItemPosition;
 
     @Override
@@ -16,7 +35,20 @@ public class ViewItemActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_item);
 
+        ButterKnife.bind(this);
+
         Intent mIntent = getIntent();
         mItemPosition = mIntent.getIntExtra(Constants.ITEM_POSITION, 0);
+
+        Item mSelectedItem = ItemsStorage.getInstance().getItem(mItemPosition);
+
+        completeData(mSelectedItem);
+    }
+
+    private void completeData(Item item) {
+        mAquery.id(R.id.imageInViewItem).image(item.getmImageLink());
+        mTitle.setText(item.getmTitle());
+        mDate.setText(item.getmPubDate());
+        mDescription.setText(item.getmDescription());
     }
 }
