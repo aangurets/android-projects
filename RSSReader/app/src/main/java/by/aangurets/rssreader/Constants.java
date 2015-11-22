7c386c861ac5b46e67c5189a79cf337e83040387
Andrei Angurets <andrei.angurets@gmail.com>
2015-11-22 23:43:02 +0300

    small changes

--- RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
@@ -8,4 +8,5 @@ public class Constants {
     public final static String FAKTY_URL = "http://www.rmf24.pl/fakty/feed";
     public final static String LOG_TAG = "OWN.LOG";
     public final static String ITEM_POSITION = "Item position";
+    public final static String URL = "Url";
 }
