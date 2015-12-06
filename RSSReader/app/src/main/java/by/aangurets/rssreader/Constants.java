4057b922c44f2d251ec55034da8fa34a015f7b89
Andrei Angurets <andrei.angurets@gmail.com>
2015-12-06 22:28:00 +0300

    add endpoint & fakty path

--- RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
@@ -5,7 +5,9 @@ package by.aangurets.rssreader;
  */
 public class Constants {
 
+    public final static String RMF_ENDPOINT = "http://www.rmf24.pl";
     public final static String FAKTY_URL = "http://www.rmf24.pl/fakty/feed";
+    public final static String PATH_FAKTY = "fakty";
     public final static String LOG_TAG = "OWN.LOG";
     public final static String ITEM_POSITION = "Item position";
     public final static String URL = "Url";
