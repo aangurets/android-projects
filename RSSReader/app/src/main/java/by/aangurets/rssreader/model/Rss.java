6d5e7135fab8351a2e39b51735921dde5eb4a729
Andrei Angurets <andrei.angurets@gmail.com>
2015-12-06 19:53:16 +0300

    add rss class

---
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/Rss.java
@@ -0,0 +1,20 @@
+package by.aangurets.rssreader.model;
+
+import org.simpleframework.xml.Root;
+
+/**
+ * Created by aangurets on 06/12/2015.
+ */
+
+@Root(name = "rss")
+public class Rss {
+
+    private Channel mChannel;
+
+    public Rss() {
+    }
+
+    public Rss(Channel mChannel) {
+        this.mChannel = mChannel;
+    }
+}
