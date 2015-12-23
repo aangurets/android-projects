4b3dc39deb7db7cc5c3524303e6301e3bf1ae02a
Andrei <Andrei>
2015-12-23 23:23:47 +0300

    del other fields from itemsList

--- RSSReader/app/src/main/java/by/aangurets/rssreader/model/ItemsList.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/ItemsList.java
@@ -1,23 +1,13 @@
 package by.aangurets.rssreader.model;
 
-import org.simpleframework.xml.Attribute;
-import org.simpleframework.xml.Element;
 import org.simpleframework.xml.ElementList;
-import org.simpleframework.xml.Root;
 
 import java.util.ArrayList;
 
 /**
  * Created by aangurets on 08/12/2015.
  */
-@Root
 public class ItemsList extends ArrayList<Item> {
-
-    @Attribute(name = "version")
-    private String mVersion;
-
-    @Element(name = "channel")
-    private Channel mChannel;
 
     @ElementList(required = false)
     ArrayList<Item> items;
