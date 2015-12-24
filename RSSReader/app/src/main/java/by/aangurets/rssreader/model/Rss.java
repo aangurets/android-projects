dcb5a257ad1c73fed056e7787e7d0f3eea44dedc
aangurets <andrei.angurets@gmail.com>
2015-12-24 09:48:06 +0200

    add attribute to rss class

--- RSSReader/app/src/main/java/by/aangurets/rssreader/model/Rss.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/Rss.java
@@ -1,5 +1,6 @@
 package by.aangurets.rssreader.model;
 
+import org.simpleframework.xml.Attribute;
 import org.simpleframework.xml.Element;
 import org.simpleframework.xml.Root;
 
@@ -10,9 +11,39 @@ import org.simpleframework.xml.Root;
 @Root(name = "rss", strict = false)
 public class Rss {
 
+    @Attribute(name = "atom", required = false)
+    private String atom;
+
+    @Attribute(name = "version", required = false)
+    private String version;
+
     @Element(name = "channel")
     private Channel mChannel;
 
     public Rss() {
+    }
+
+    public String getAtom() {
+        return atom;
+    }
+
+    public void setAtom(String atom) {
+        this.atom = atom;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public Channel getmChannel() {
+        return mChannel;
+    }
+
+    public void setmChannel(Channel mChannel) {
+        this.mChannel = mChannel;
     }
 }
