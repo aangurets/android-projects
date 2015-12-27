2d8d48f49a2ef4ff66f45ecc689362293b7753fa
aangurets <andrei.angurets@gmail.com>
2015-12-27 11:25:25 +0300

    clear items list class

--- RSSReader/app/src/main/java/by/aangurets/rssreader/model/ItemsList.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/model/ItemsList.java
@@ -6,17 +6,4 @@ import java.util.ArrayList;
  * Created by aangurets on 08/12/2015.
  */
 public class ItemsList extends ArrayList<Item> {
-
-    ArrayList<Item> items;
-
-    public ItemsList() {
-    }
-
-    public ArrayList<Item> getmItems() {
-        return items;
-    }
-
-    public void setmItems(ArrayList<Item> mItems) {
-        this.items = mItems;
-    }
 }
