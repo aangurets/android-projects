48484db075ac7a2efb5142bb85c4723aa9ce910f
aangurets <andrei.angurets@gmail.com>
2015-12-27 12:31:48 +0300

    remove fakty url

--- RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
+++ RSSReader/app/src/main/java/by/aangurets/rssreader/Constants.java
@@ -6,7 +6,6 @@ package by.aangurets.rssreader;
 public class Constants {
 
     public final static String RMF_ENDPOINT = "http://www.rmf24.pl";
-    public final static String FAKTY_URL = "http://www.rmf24.pl/fakty/feed";
     public final static String PATH_FAKTY = "fakty";
     public final static String LOG_TAG = "OWN.LOG";
     public final static String ITEM_POSITION = "Item position";
